246af4e600de372d35795e32cb95dd397523a71c
chj <bluedog129@gmail.com>
2022-07-18 12:12:28 +0900

    exam do_op

--- ft_codes/exam/lv02/do_op.c
+++ ft_codes/exam/lv02/do_op.c
@@ -12,22 +12,24 @@ int main(int ac, char **av)
     result = 0;
     if (ac == 4)
     {
-        a = atoi(av[1]);
-        op = av[2][0];
-        b = atoi(av[3]);
-        if (op == '+')
-			result = a + b;
-        else if (op == '-')
-			result = a - b;
-		else if (op == '*')
-			result = a * b;
-		else if (op == '/')
-			result = a / b;
-		else if (op == '%')
-			result = a % b;
-		printf("%d\n", result);
+      // 문자열로받은 인덱스 1,3을 정수형으로 변환하기 위해 atoi로 
+      a = atoi(av[1]);
+      // 인덱스 2는 op변수에 넣는다
+      op = av[2][0];
+      b = atoi(av[3]);
+      if (op == '+')
+			  result = a + b;
+      else if (op == '-')
+			  result = a - b;
+      else if (op == '*')
+        result = a * b;
+		  else if (op == '/')
+			  result = a / b;
+		  else if (op == '%')
+		  	result = a % b;
+		  printf("%d\n", result);
     }
     else
-		write(1, "\n", 1);
+		  write(1, "\n", 1);
 	return (0);
 }
